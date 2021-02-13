03a4e41c4bee6a0194fa0a305dd105507399f1a5
Kristaps <strazds@mail.ru>
2021-02-13 18:04:17 +0200

    Adding new routes

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const { connect } = require('mongoose');
+const connectDB = require('./config/db');
+
 const app = express();
 
+//Connect database
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
